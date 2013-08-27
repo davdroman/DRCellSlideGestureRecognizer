1be727dfd4b8132e1cd8d5a3a61ae2fe99f101f8
David Román <dromaguirre@icloud.com>
2013-08-27 23:46:40 +0200

    Added main view controller to example app.
    Added main view controller to example app.

---
+++ PDGesturedTableView/PDGesturedTableView/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  PDGesturedTableView
+//
+//  Created by David Román Aguirre on 27/08/13.
+//  Copyright (c) 2013 David Román Aguirre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ PDGesturedTableView/PDGesturedTableView/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  PDGesturedTableView
+//
+//  Created by David Román Aguirre on 27/08/13.
+//  Copyright (c) 2013 David Román Aguirre. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
